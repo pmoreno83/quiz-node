b0fdd01486c6eb44295571603af9da3ff59fb0a8
pmoreno83 <pmoreno83@gmail.com>
2015-08-02 20:03:43 +0200

    Se añade autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -11,21 +11,34 @@ exports.show = function (req,res){
 
 //get('/quizes/:id/answer')
 exports.answer = function (req,res){
-	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		if (req.query.respuesta === quiz.respuesta){
-			res.render('quizes/answer', { quiz:quiz, respuesta: 'Correcto'});
-		}
-		else {
-			res.render('quizes/answer', { quiz:quiz, respuesta: 'Incorrecto'});
-		}
 
-	});
+	var resultado = 'Incorrecto';
+	if (req.query.respuesta === req.quiz.respuesta){
+		resultado = 'Correcto';
+	}
 	
+	res.render('quizes/answer', { quiz:req.quiz, respuesta: resultado});
 };
 
 //get('/quizes')
 exports.index = function(req,res){
 	models.Quiz.findAll().then(function(quizes){
 		res.render('quizes/index',{quizes: quizes});
+	}).catch(function(error){
+		next(error);
 	})
+};
+
+//Autoload - factoriza el codigo si la ruta incluye :quizId
+exports.load = function(req,res,next,quizId){
+	models.Quiz.findById(quizId).then(function(quiz) {
+			if (quiz) {
+				req.quiz = quiz;
+				next();
+			} else {
+				next(new Error('No existe quizId=' + quizId));
+			}
+		}
+	).catch(function(error) { next(error); });
+
 };
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+//autoload de comandos con :quizId
+router.param('quizId', quizController.load);
+//definicacion de rutas
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);

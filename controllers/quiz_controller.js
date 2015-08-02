1ff7329f941e677a32b4fc0c368497ce014ceb7a
pmoreno83 <pmoreno83@gmail.com>
2015-08-02 19:07:06 +0200

    Se añade listado de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,24 +1,31 @@
 //importamos el modelo
 var models = require('../models/models');
 
-//get('/quizes/question')
-exports.question = function (req,res){
-	models.Quiz.findAll().then(function(quiz){
-		res.render('quizes/question', { pregunta: quiz[0].pregunta });	
+//get('/quizes/:id')
+exports.show = function (req,res){
+	models.Quiz.findById(req.params.quizId).then(function(quiz){
+		res.render('quizes/show', { quiz: quiz });	
 	});
 	
 };
 
-//get('/quizes/answer')
+//get('/quizes/:id/answer')
 exports.answer = function (req,res){
-	models.Quiz.findAll().then(function(quiz){
-		if (req.query.respuesta === quiz[0].respuesta){
-			res.render('quizes/answer', {respuesta: 'Correcto'});
+	models.Quiz.findById(req.params.quizId).then(function(quiz){
+		if (req.query.respuesta === quiz.respuesta){
+			res.render('quizes/answer', { quiz:quiz, respuesta: 'Correcto'});
 		}
 		else {
-			res.render('quizes/answer', {respuesta: 'Incorrecto'});
+			res.render('quizes/answer', { quiz:quiz, respuesta: 'Incorrecto'});
 		}
 
 	});
 	
+};
+
+//get('/quizes')
+exports.index = function(req,res){
+	models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/index',{quizes: quizes});
+	})
 };
--- models/models.js
+++ models/models.js
@@ -40,6 +40,11 @@ sequelize.sync().then(function() {
 				{
 					pregunta: 'Capital de Italia',
 					respuesta: 'Roma'
+				});
+			Quiz.create(
+				{
+					pregunta: 'Capital de Portugal',
+					respuesta: 'Lisboa'
 				}).then(function(){
 					console.log('Base de datos inicializada')
 				});
